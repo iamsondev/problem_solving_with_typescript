a967ccbd173c88ae9556fa11ee81ed63504d8447
iamsondev <sondipkumarsk@gmail.com>
2025-11-18 01:25:29 +0600

    add filterByRating function using interface and generic for reusable

--- src/solution.ts
+++ src/solution.ts
@@ -1,4 +1,3 @@
-//  ******************problem-01**********************
 const formateValue = <T>(value: T): string | number | boolean => {
   if (typeof value === "string") {
     return value.toUpperCase();
@@ -12,8 +11,6 @@ const formateValue = <T>(value: T): string | number | boolean => {
   throw new Error("unsupported type ");
 };
 
-// ******************problem-2*****************************
-
 const getLength = (value: string | any[]): number => {
   if (typeof value === "string") {
     return value.length;
@@ -23,8 +20,6 @@ const getLength = (value: string | any[]): number => {
   }
   return 0;
 };
-
-// *******************problem-03******************
 
 class Person {
   name: string;
@@ -40,7 +35,22 @@ class Person {
 }
 
 const person1 = new Person("John Doe", 30);
-console.log(person1.getDetails());
 
 const person2 = new Person("Alice", 25);
-console.log(person2.getDetails());
+
+interface Item {
+  title: string;
+  rating: number;
+}
+
+const filterByRating = <T extends { rating: number }>(items: T[]) => {
+  return items.filter((item) => item.rating >= 4);
+};
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+console.log(filterByRating(books));
