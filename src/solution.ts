b03bd310fbe4ac3e22cdaa1258a7a0be4af52e1d
iamsondev <sondipkumarsk@gmail.com>
2025-11-17 23:09:38 +0600

    add getLength function , handle the typeof and length

--- src/solution.ts
+++ src/solution.ts
@@ -12,6 +12,17 @@ const formateValue = <T>(value: T): string | number | boolean => {
   throw new Error("unsupported type ");
 };
 
-console.log(formateValue("gold"));
-console.log(formateValue(5));
-console.log(formateValue(true));
+// ******************problem-2*****************************
+
+const getLength = (value: string | any[]): number => {
+  if (typeof value === "string") {
+    return value.length;
+  }
+  if (Array.isArray(value)) {
+    return value.length;
+  }
+  return 0;
+};
+
+console.log(getLength("typescript"));
+console.log(getLength([10, 20, 30, 40]));
